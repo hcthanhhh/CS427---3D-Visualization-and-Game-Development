b51087618f60b843f37aa198479fe160e18fcb43
An 'HAKONY' Phạm <32831923+makonhakony@users.noreply.github.com>
2019-06-02 21:31:08 +0700

    A02 submission
    A02 submission

---
+++ CS427_Group09/A02/Assets/Spawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject Player; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating ("spawn",0,3); 
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void spawn()
+    {
+        GameObject newPlayer = (GameObject) Instantiate (Player) as GameObject;
+        float x = Random.Range(-5,5);
+        float y = Random.Range(-2,2);
+
+        newPlayer.transform.position = new Vector2(x,y);
+
+    }
+}
