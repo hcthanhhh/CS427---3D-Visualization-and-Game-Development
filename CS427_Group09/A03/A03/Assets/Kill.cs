eed6d198d2c2db228e78c8982c11220ab4ddf992
Thanh Chan Hong <hcthanh@users.noreply.github.com>
2019-06-12 22:46:50 +0700

    add Dead

---
+++ CS427_Group09/A03/A03/Assets/Kill.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill : MonoBehaviour
+{
+    void Update(){
+    }
+    void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.tag == "Player"){
+            // collision.gameObject.SendMessage("Dead", F);
+            Destroy(collision.gameObject);
+        }
+    }
+}
